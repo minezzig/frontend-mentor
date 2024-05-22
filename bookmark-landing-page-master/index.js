34f971849ebcc59a421f297a77d145a42200fcd1
Greg <gregoryminezzi@gmail.com>
2024-05-22 14:50:02 +0200

    implement feature tab focus

--- bookmark-landing-page-master/index.js
+++ bookmark-landing-page-master/index.js
@@ -1,23 +1,48 @@
 const hamburger = document.getElementById("hamburger");
 const close = document.getElementById("close");
 const dropdown = document.getElementById("dropdown");
+const tabs = document.querySelectorAll("[data-tab]");
 const questions = document.querySelectorAll("#question");
 const email = document.getElementById("email");
 const error = document.getElementById("error");
 const submit = document.getElementById("submit");
 
+// create function mobile navigation
 hamburger.addEventListener("click", () => {
   dropdown.classList.add("flex");
   dropdown.classList.remove("hidden");
   document.body.classList.add("overflow-y-hidden");
 });
 
+// close mobile navigation
 close.addEventListener("click", () => {
   dropdown.classList.remove("flex");
   dropdown.classList.add("hidden");
   document.body.classList.remove("overflow-y-hidden");
 });
 
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    // on click, hide all
+    tabs.forEach((tab) => {
+      const curTab = document.getElementById(`${tab.dataset.tab}`);
+      curTab.classList.add("hidden");
+      curTab.classList.remove("flex");
+      //tab.classList.remove("border-red");
+      //also remove inital tab
+      tab.classList.remove("after:absolute");
+    });
+    // only show the one that was clicked
+    const clickedTab = document.getElementById(`${tab.dataset.tab}`);
+    clickedTab.classList.add("flex");
+    clickedTab.classList.remove("hidden");
+    //tab.classList.toggle("border-red");
+    // tab.classList.toggle("border-6");
+    tab.classList.add("after:absolute");
+  });
+});
+
+// FAQ section - dropdown answer and animate arrows
 questions.forEach((question) => {
   question.addEventListener("click", () => {
     const arrow = question.childNodes[1];
@@ -30,6 +55,7 @@ questions.forEach((question) => {
   });
 });
 
+// email validation on submit
 submit.addEventListener("click", () => {
   if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value))
     error.classList.remove("hidden");
