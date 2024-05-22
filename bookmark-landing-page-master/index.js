cd99e1859d9671a1d9db9cc26e30e0e2d5a15393
Greg <gregoryminezzi@gmail.com>
2024-05-22 13:16:48 +0200

    change arrow SVG color on dropdown

--- bookmark-landing-page-master/index.js
+++ bookmark-landing-page-master/index.js
@@ -1,36 +1,37 @@
 const hamburger = document.getElementById("hamburger");
 const close = document.getElementById("close");
 const dropdown = document.getElementById("dropdown");
-const questions = document.querySelectorAll("#question")
+const questions = document.querySelectorAll("#question");
 const email = document.getElementById("email");
-const error = document.getElementById("error")
+const error = document.getElementById("error");
 const submit = document.getElementById("submit");
 
 hamburger.addEventListener("click", () => {
-    dropdown.classList.add("flex")
-    dropdown.classList.remove("hidden")
-    document.body.classList.add("overflow-y-hidden")
+  dropdown.classList.add("flex");
+  dropdown.classList.remove("hidden");
+  document.body.classList.add("overflow-y-hidden");
 });
 
 close.addEventListener("click", () => {
-    dropdown.classList.remove("flex");    
-    dropdown.classList.add("hidden")
-    document.body.classList.remove("overflow-y-hidden")
+  dropdown.classList.remove("flex");
+  dropdown.classList.add("hidden");
+  document.body.classList.remove("overflow-y-hidden");
 });
 
-questions.forEach(question => {
-    question.addEventListener("click", () => {
-        const arrow = question.childNodes[1];
-        const answer = question.nextSibling.nextSibling;
-        arrow.classList.toggle("rotate-180");
-        answer.classList.toggle("max-h-96")
-        arrow.parentElement.nextElementSibling.classList.toggle("opacity-100");
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const arrow = question.childNodes[1];
+    const answer = question.nextSibling.nextSibling;
 
-    });
+    arrow.classList.toggle("rotate-180");
+    arrow.childNodes[1].classList.toggle("stroke-red");
+    answer.classList.toggle("max-h-96");
+    answer.classList.toggle("opacity-100");
+  });
 });
 
 submit.addEventListener("click", () => {
-    if(!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value)) error.classList.remove("hidden");
-    else error.classList.add("hidden")
-
+  if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value))
+    error.classList.remove("hidden");
+  else error.classList.add("hidden");
 });
