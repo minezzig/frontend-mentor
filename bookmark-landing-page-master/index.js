a30197904b3e04588ae9390d46cab32099f3b2d2
Greg <gregoryminezzi@gmail.com>
2024-05-22 13:10:22 +0200

    dropdown when click question

--- bookmark-landing-page-master/index.js
+++ bookmark-landing-page-master/index.js
@@ -1,8 +1,7 @@
 const hamburger = document.getElementById("hamburger");
 const close = document.getElementById("close");
 const dropdown = document.getElementById("dropdown");
-//const answers = document.querySelectorAll("#answers")
-const arrows = document.querySelectorAll("#arrow");
+const questions = document.querySelectorAll("#question")
 const email = document.getElementById("email");
 const error = document.getElementById("error")
 const submit = document.getElementById("submit");
@@ -19,15 +18,16 @@ close.addEventListener("click", () => {
     document.body.classList.remove("overflow-y-hidden")
 });
 
-
-arrows.forEach(arrow => {
-    arrow.addEventListener("click", () => {
+questions.forEach(question => {
+    question.addEventListener("click", () => {
+        const arrow = question.childNodes[1];
+        const answer = question.nextSibling.nextSibling;
         arrow.classList.toggle("rotate-180");
-        arrow.classList.toggle("stroke-red")
-        arrow.parentElement.nextElementSibling.classList.toggle("max-h-96");
+        answer.classList.toggle("max-h-96")
         arrow.parentElement.nextElementSibling.classList.toggle("opacity-100");
-    })
-})
+
+    });
+});
 
 submit.addEventListener("click", () => {
     if(!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value)) error.classList.remove("hidden");
