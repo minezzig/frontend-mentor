f87f5a07cdacaab511494aba0295eaa2c14fd63c
Greg <gregoryminezzi@gmail.com>
2024-05-22 12:56:05 +0200

    add email validation and edit hover effects

--- bookmark-landing-page-master/index.js
+++ bookmark-landing-page-master/index.js
@@ -1,8 +1,11 @@
-const hamburger = document.getElementById("hamburger")
-const close = document.getElementById("close")
-const dropdown = document.getElementById("dropdown")
+const hamburger = document.getElementById("hamburger");
+const close = document.getElementById("close");
+const dropdown = document.getElementById("dropdown");
 //const answers = document.querySelectorAll("#answers")
-const arrows = document.querySelectorAll("#arrow")
+const arrows = document.querySelectorAll("#arrow");
+const email = document.getElementById("email");
+const error = document.getElementById("error")
+const submit = document.getElementById("submit");
 
 hamburger.addEventListener("click", () => {
     dropdown.classList.add("flex")
@@ -25,3 +28,9 @@ arrows.forEach(arrow => {
         arrow.parentElement.nextElementSibling.classList.toggle("opacity-100");
     })
 })
+
+submit.addEventListener("click", () => {
+    if(!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value)) error.classList.remove("hidden");
+    else error.classList.add("hidden")
+
+});
