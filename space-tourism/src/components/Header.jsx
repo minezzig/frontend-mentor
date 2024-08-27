1d5bdd853357299615931066517cef1cb7adda5e
Greg <gregoryminezzi@gmail.com>
2024-08-27 10:59:54 +0200

    fix header respnsiveness

--- space-tourism/src/components/Header.jsx
+++ space-tourism/src/components/Header.jsx
@@ -16,9 +16,12 @@ export const Header = () => {
       <div className="hamburger" onClick={toggleMenu}>
         <img src={`../../src/assets/shared/icon-${menuOpen ? 'close' : 'hamburger'}.svg`} alt="menu" />
       </div>
+      <div className="desktop-bar">
+        <hr/>
+      </div>
       <nav className={`${menuOpen && 'open'}`}>
         <ul className="nav-text">
-          <li><a href="/" title="home">home</a></li>
+          <li><a href="/" title="home"><span>00 </span>home</a></li>
           <li><a href="/destination" title="destination"><span>01 </span>destination</a></li>
           <li><a href="/crew" title="crew"><span>02 </span>crew</a></li>
           <li><a href="/technology" title="technology"><span>03 </span>technology</a></li>
